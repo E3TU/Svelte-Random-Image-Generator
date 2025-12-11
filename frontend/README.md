1052b3e88e50039f29528b3a6e8b7716ede9a6b1
Eetu <99178022+E3TU@users.noreply.github.com>
2025-12-11 20:16:41 +0200

    Initialized project

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+# Svelte-Random-Image-Generator
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<div class="main-container">
+	<h1>Random Image Generator</h1>
+</div>
+
+<style>
+    .main-container{
+        display: flex;
+        width: 100%;
+        height: 100%;
+        align-items: center;
+        justify-content: center;
+    }
+	h1 {
+        font-size: 2.2rem;
+		color: white;
+	}
+</style>
