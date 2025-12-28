f6faa604ae02c0dcf815f63c6ccaa6cbc828e525
Eetu <99178022+E3TU@users.noreply.github.com>
2025-12-28 22:19:56 +0200

    Moved globalcss file

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-	import '../../styles/global.css';
+	import '../styles/global.css';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
 </svelte:head>
 
 {@render children()}
