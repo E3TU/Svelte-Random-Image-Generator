4e99e66800c56e9de194b9cfd524136c9302825d
Eetu <99178022+E3TU@users.noreply.github.com>
2026-01-01 23:17:08 +0200

    Changed button styling + animation

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,28 +1,31 @@
 <script lang="ts">
 	let imageUrl: string;
 
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 
-	async function fetchData() {
+	async function fetchImage() {
 		let url: string = 'http://localhost:5199/randomphoto';
 
 		const res = await fetch(url);
 
-        const data = await res.json();
+		const data = await res.json();
 
-        console.log(data);
+		console.log(data);
 
 		imageUrl = data.photoUrl;
 	}
 
-    onMount(() => {
-        fetchData();
-    });
+	onMount(() => {
+		fetchImage();
+	});
 </script>
 
 <div class="main-container">
 	<h1>Random Image Generator</h1>
-	<img src={imageUrl}>
+	<div class="image-container">
+		<img alt="random photo from unsplash" src={imageUrl} />
+	</div>
+	<button id="generate-image" on:click={fetchImage} >Generate Image</button>
 </div>
 
 <style>
@@ -31,10 +34,51 @@
 		width: 100%;
 		height: 100%;
 		align-items: center;
-		justify-content: center;
+		/* justify-content: center; */
+		flex-direction: column;
 	}
 	h1 {
 		font-size: 2.2rem;
+		color: #76d5e1;
+	}
+	.image-container {
+		width: 80rem;
+		height: 50rem;
+		position: relative;
+		overflow: hidden;
+		box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+		border-radius: 12px;
+		border: 3px solid white;
+	}
+	.image-container img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+	#generate-image {
+		cursor: pointer;
+		border: 4px solid #76d5e1;
 		color: white;
+		background-color: #353535;
+		padding: 2rem 4rem;
+		margin-top: 2rem;
+		border-radius: 8px;
+		font-size: 1.25rem;
+		box-shadow:
+			rgba(0, 0, 0, 0.25) 0px 54px 55px,
+			rgba(0, 0, 0, 0.12) 0px -12px 30px,
+			rgba(0, 0, 0, 0.12) 0px 4px 6px,
+			rgba(0, 0, 0, 0.17) 0px 12px 13px,
+			rgba(0, 0, 0, 0.09) 0px -3px 5px;
+		transition: 0.5s ease;
+	}
+	#generate-image:hover {
+		background-color: #76d5e1;
+		color: black;
+		transition: 0.5s ease;
+	}
+	#generate-image:active {
+		transform: scale(0.90);
+		transition: 0.3s;
 	}
 </style>
