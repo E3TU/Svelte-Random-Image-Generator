3de2fbc96d65144df707314208f51c7ef60407eb
Eetu <99178022+E3TU@users.noreply.github.com>
2026-01-01 22:22:59 +0200

    Get request to fetch image

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,17 +1,40 @@
+<script lang="ts">
+	let imageUrl: string;
+
+	import { onMount } from "svelte";
+
+	async function fetchData() {
+		let url: string = 'http://localhost:5199/randomphoto';
+
+		const res = await fetch(url);
+
+        const data = await res.json();
+
+        console.log(data);
+
+		imageUrl = data.photoUrl;
+	}
+
+    onMount(() => {
+        fetchData();
+    });
+</script>
+
 <div class="main-container">
 	<h1>Random Image Generator</h1>
+	<img src={imageUrl}>
 </div>
 
 <style>
-    .main-container{
-        display: flex;
-        width: 100%;
-        height: 100%;
-        align-items: center;
-        justify-content: center;
-    }
+	.main-container {
+		display: flex;
+		width: 100%;
+		height: 100%;
+		align-items: center;
+		justify-content: center;
+	}
 	h1 {
-        font-size: 2.2rem;
+		font-size: 2.2rem;
 		color: white;
 	}
 </style>
